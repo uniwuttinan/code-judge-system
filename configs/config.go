4bb5455857f5f7ef26f3e51f390a96e39fbbdaad
uniwuttinan <117209788+uniwuttinan@users.noreply.github.com>
2024-01-24 07:46:05 +0000

    update way to load env files

--- configs/config.go
+++ configs/config.go
@@ -2,22 +2,32 @@ package configs
 
 import (
 	"log"
+	"os"
 
 	"github.com/spf13/viper"
 )
 
 func LoadConfig() {
-	var err error
-
-	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("../")
 
-	err = viper.ReadInConfig()
-	if err == nil {
-		log.Println("Using .env file")
+	// Load .env.local if it exists
+	if _, err := os.Stat(".env.local"); err == nil {
+		viper.SetConfigName(".env.local")
+		err = viper.ReadInConfig()
+		if err != nil {
+			log.Println("failed to load .env.local file")
+		}
+	} else {
+		// Load .env if .env.local does not exist or failed to load
+		viper.SetConfigName(".env")
+		err = viper.ReadInConfig()
+		if err != nil {
+			log.Println("failed to load .env file")
+		}
 	}
 
+	// Load environment variables from OS environment
 	viper.AutomaticEnv()
 }
